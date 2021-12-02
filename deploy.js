29f8add3a7c6401209020920d18b08ed698837f9
City of Vernonia GIS <gis@vernonia-or.gov>
2021-12-02 09:15:39 -0800

    add headers

--- deploy.js
+++ deploy.js
@@ -7,3 +7,5 @@ const fs = require('fs-extra');
 fs.copy('docs', 'dist/docs');
 fs.copy('index.html', 'dist/index.html');
 fs.copy('vernonia-annexations.geojson', 'dist/vernonia-annexations.geojson');
+fs.copy('_headers', 'dist/_headers');
+fs.copy('_redirects', 'dist/_redirects');
